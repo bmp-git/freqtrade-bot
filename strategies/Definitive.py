bc9ed5ef4d5bc7bbb27d9f0c37fe53cadb46f8a3
Edoardo Barbieri <edoardo.barbieri.96@gmail.com>
2021-01-02 16:50:12 +0100

    First commit

---
+++ strategies/Definitive.py
@@ -0,0 +1,39 @@
+
+# --- Do not remove these libs ---
+from freqtrade.strategy.interface import IStrategy
+from typing import Dict, List
+from functools import reduce
+from pandas import DataFrame
+# --------------------------------
+
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+import numpy # noqa
+
+
+class Definitive(IStrategy):
+    minimal_roi = {
+        "0": 0.05
+    }
+
+    stoploss = -0.25
+
+    timeframe = '5m'
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+            ),
+            'buy'] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+            ),
+            'sell'] = 1
+        return dataframe
